a24f828965af505358e5ceffd3b20e2b800e108e
zeus-kronos <zeuskronorz.com>
2022-09-08 23:43:00 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *   *                followed by a new line, to standard error.
+ *    *
+ *     * Return: Always 1.
+ *      */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
 
-	return (1);
+		return (1);
 }
