d2a87b4d3e691674fb3314bab3d045b78893de38
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-12 23:31:06 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Zeus
+ */
+
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
 */
-
 int main(void)
 {
 	int n;
@@ -17,12 +22,10 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	
-	else
+	else if (n < 0)
 		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 
 	return (0);
 }
