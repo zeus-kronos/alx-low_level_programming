2d143b54d3f2198b063315cc3a65534bcbeb2e03
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-16 03:43:41 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,9 @@
 #include "main.h"
-
 /**
- * _isupper - uppercase letters
- * @c: character to check
- *
- * Return: 0 or 1
+ * _isupper - check the code for Holberton School students.
+ * @c: Character input
+ * Return: Always 0.
  */
-
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
