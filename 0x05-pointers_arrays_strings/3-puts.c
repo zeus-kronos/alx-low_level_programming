654f16b58ea443fc88bd98480746f095b9c54c5b
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-20 02:10:58 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of a string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
