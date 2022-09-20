ae0587d677253383b5445f23b0c8aac249ffcaac
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-20 02:06:04 +0100

    Update 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: parameter to take the value.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
