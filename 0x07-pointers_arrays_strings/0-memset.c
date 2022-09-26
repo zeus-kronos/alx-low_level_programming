f3083c11d398fdd232e710dbcb008562632906c0
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 23:16:26 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
-*_memset - The _memset() function fills
-* the first n bytes of the memory area
-* pointed to by s with the constant byte b
-*@s:target
-*@b: constant byte
-*@n:number of byte
-*Return: returns new value of target
+* *_memset - fills memory with a constant byte.
+* @s: pointer to put the constant
+* @b: constant
+* @n: max bytes to use
+* Return: s
 */
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
-	return (s);
+unsigned int i;
 
+for (i = 0; n > 0 ; i++, n--)
+{
+	s[i] = b;
+}
+
+return (s);
 }
