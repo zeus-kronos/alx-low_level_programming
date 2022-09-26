f52def9b90ee0ccc5da2b85164ee7f9469a084dd
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 21:40:27 +0100

    Create main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(chr *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int main(void);
+int _putchar(char c);
