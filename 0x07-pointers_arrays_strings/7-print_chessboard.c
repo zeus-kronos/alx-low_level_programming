a96e18c107079fde02972fb6d8d0272aa3b953d8
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 23:28:16 +0100

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_chessboard - Prints chessboard
+*
+* @a: Double pointer
+*
+* Return: 0
+*/
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
