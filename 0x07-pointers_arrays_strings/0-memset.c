1cc5a2ccd06c58b7729685e6699a3a76f899fef7
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 20:44:54 +0100

    Update 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * _memset - fills memory with a constant byte
+  * @s: char
+  * @b: char
+  * @n: unsigned int
+  * Return: char*
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int x;
+
+for (x = 0; x < n; x++)
+{
+	s[x] = b;
+
+}
+return (s);
+}
