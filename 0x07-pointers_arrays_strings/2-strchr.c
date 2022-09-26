1dfd0079b3255c450881b27fb925ee79749014fb
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 23:25:00 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strchr - Loctes a character in a string
- *
- * @s: The string to check
- *
- * @c: The character to find
- *
- * Return: Pointer to @s or NULL
- *
- */
-
+* _strchr - Loctes a character in a string
+*
+* @s: The string to check
+*
+* @c: The character to find
+*
+* Return: Pointer to @s or NULL
+*
+*/
 char *_strchr(char *s, char c)
 {
 	while (*s)
