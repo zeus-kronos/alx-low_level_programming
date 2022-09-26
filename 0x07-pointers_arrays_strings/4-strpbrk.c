9b47eed28b5b8c8b21fac1f6b4c4125818bf981c
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 21:46:47 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to check
- * @accept: string to check against
- *
- * Return: pointer to byte in s that matches or NULL if no match
- */
+* _strpbrk - searches a string for any of a set of bytes
+* @s: string to check
+* @accept: string to check against
+*
+* Return: pointer to byte in s that matches or NULL if no match
+*/
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
