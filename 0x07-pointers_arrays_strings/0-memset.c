f859a2144a861f8f56e41d41203bb9b493daaac3
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 22:07:08 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
-* _memset - fills memory with a constant byte
-* @s: memory space to be filled
-* @b: byte to fill with
-* @n: number of spaces to fill
-*
-* Return: pointer to memory area s
+* _memset -> this is memory set function
+* @s: string
+* @b: character
+* @n: integer
+* Return: a string
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
 	return (s);
 }
