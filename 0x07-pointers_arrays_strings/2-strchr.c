f30eb95791126eac916c5afb285b66598b9bba39
zeus-kronos <111608696+zeus-kronos@users.noreply.github.com>
2022-09-26 23:23:48 +0100

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Loctes a character in a string
+ *
+ * @s: The string to check
+ *
+ * @c: The character to find
+ *
+ * Return: Pointer to @s or NULL
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		s++;
+
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+
+	return (0);
+}
