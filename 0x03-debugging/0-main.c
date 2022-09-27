7a20b02436ec5fca24557d997f36b2445d65080d
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-27 02:27:52 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -7,8 +7,10 @@
 
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
