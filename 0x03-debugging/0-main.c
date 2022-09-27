46d7dcbce7b35f151d7d78bd87b3c7ddbdf4ec3e
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-27 02:25:51 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-
-int main(void)
-{
-	int i;
-	
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
-=======
 #include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -32,4 +13,3 @@ int main(void)
 
 	return (0);
 }
->>>>>>> 4ca3a813e8766f3a7eb1c577b8e2ef1373fabfa9
