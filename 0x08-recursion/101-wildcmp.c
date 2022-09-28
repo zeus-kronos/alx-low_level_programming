939749d93d413d83b1446aece7e413e78b5e0151
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-28 11:20:25 +0100

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* wildcmp - Compare two strings allowing for wildcard char
+*
+* @s1: String being compared
+*
+* @s2: String being compared against
+*
+* Return: 1 if considered identical, 0 otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
