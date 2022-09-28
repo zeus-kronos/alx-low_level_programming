72dfc7aba8f1fa22f8209ce05c5e9bcec2dda082
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-28 11:21:29 +0100

    Create main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+
+#endif
