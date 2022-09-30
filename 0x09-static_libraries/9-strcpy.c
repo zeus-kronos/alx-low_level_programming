5b91143916db8c8f6a349b3e23fa7e1e0da20230
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:43:50 +0100

    Create 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcpy - Copy strings from source arr to destination arr
+*
+* @dest: destination array
+* @src: source array
+*
+* Return: Pointer to destination array
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
