7c8b8d9e38627a54e3aee7b6cc9326991b278a74
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:32:45 +0100

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _islower - Checks for lowercase character
+*
+* @c: Number to be checked
+*
+* Return: 1 (Success) OR 0 (Failure)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
