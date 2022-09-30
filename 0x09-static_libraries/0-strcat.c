798d68d210befc953870a3e35f56b8a0b3345d1b
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:22:29 +0100

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+*
+* @dest: destination string; src string is appended to it
+*
+* @src: Source string; gets appended to dest string
+*
+* Return: Pointer to dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		*(dest + i) = src[j];
+		j++;
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
