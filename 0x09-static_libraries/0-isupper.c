3d11d3b55ac1850c0973b49b19708b850794813b
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:19:17 +0100

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isupper - Checks for uppercase characters
+*
+* @c: Number to be checked
+*
+* Return: 1 (Sucess) OR 0 (Failure)
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
