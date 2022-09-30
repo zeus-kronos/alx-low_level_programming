5d9226a0c9403732ff0fade664d2b2f18d93d38e
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:23:40 +0100

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isdigit - Checks for numbers
+*
+* @c: character to checked
+*
+* Return: 1 OR 0
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
