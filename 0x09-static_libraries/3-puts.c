96676a7b4a7cb20fdddf5b196aa2fd05d2a2e9dc
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:33:50 +0100

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* _puts - Print a string folllowed by a new line.
+*
+* @str: String to be printed
+*
+* Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
