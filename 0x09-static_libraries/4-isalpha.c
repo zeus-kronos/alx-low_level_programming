da42a1835a4f491cd61de968ef6cf40d4e42e945
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:37:41 +0100

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isalpha - Checks for Alphabetic Characters
+*
+* @c: Alphabetic character
+*
+* Return: 1 (Success) OR 0 (Failure)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
