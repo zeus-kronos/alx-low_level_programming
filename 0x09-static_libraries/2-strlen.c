c70e63faccedda3dabf02698d2fcc26f6a0322bd
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:29:37 +0100

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strlen - Returns the length of a string
+*
+* @s: The character containing the string
+*
+* Return: The length of the character
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
