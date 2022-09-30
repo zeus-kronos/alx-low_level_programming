e8f8cde99ce26b917d2faae318f6121257972969
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-09-30 11:35:06 +0100

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcmp - Compare two strings
+*
+* @s1: First string
+*
+* @s2: Second string
+*
+* Return: 0 if equal, less than 1 or greater than 1
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff = 0;
+	int i = 0;
+
+	while (s1[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = ((s1[i] - '\0') - (s2[i] - '\0'));
+			break;
+		}
+		i++;
+	}
+	return (diff);
+}
