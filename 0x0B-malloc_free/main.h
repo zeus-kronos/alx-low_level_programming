5636f8ea91133fa54039472bf8cdaf19bc84d879
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-03 15:31:42 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-_putchar(char c);
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
