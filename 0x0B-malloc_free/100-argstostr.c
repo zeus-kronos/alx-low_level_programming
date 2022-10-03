34154d24d6060a10a395621a38d5baca6187e56b
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-03 15:05:05 +0100

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+/**
+* argstostr - Concatenates all the arguments of the program
+*
+* @ac: Argument total count
+*
+* @av: Pointer to arguments
+*
+* Retunr: Pointer to concatenated string (SUCCESS) or
+* NULL if @ac == 0 or @av == NULL (FAILURE) or
+* NULL if if insufficient memory was available (FAILURE)
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int count = 0;
+	int t_count = 0;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+		return ('\0');
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			t_count++;
+
+		t_count++;
+	}
+
+	result = malloc(sizeof(char) * t_count + 1);
+
+	if (result == NULL)
+	{
+		return ('\0');
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			result[count++] = av[i][j];
+		}
+		result[count++] = '\n';
+	}
+
+	result[t_count] = '\0';
+	return (result);
+}
