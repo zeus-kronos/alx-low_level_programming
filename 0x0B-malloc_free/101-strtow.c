2f81db18e55bd50d0694efe487d8db4587cc2da7
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-03 18:11:14 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,67 +1,59 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
-* _isspace - check if a character is whitespace
-* @c: the character to check
-*
-* Return: 1 is c is a whitespace character, otherwise 0
-*/
-
-int _isspace(int c)
-{
-	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
-		return (1);
-	return (0);
-}
-
-/**
-* strtow - split a string into words
-* @str: a pointer to the string to split
-*
-* Return: NULL if memory allocation fails or if str is NULL or empty (""),
-* otherwise return a pointer to the array of words terminated by a NULL
+* strtow - splits a string to words
+* @str: string to split
+* Return: a point to an array of strings or NULL
 */
 
 char **strtow(char *str)
 {
-	char **words, *pos = str;
-	int w = 0, c;
+	char **arr_words = NULL;
+	int i, j = 0, wlen, slen, words = 0, sig = 0, pre_sig = 0;
 
-	if (!(str && *str))
+	if (str == NULL)
 		return (NULL);
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		while (*(++pos) && !_isspace(*pos))
-			;
-	} while (++w, *pos);
-	if (!w)
+	slen = strlen(str);
+	for (i = 0; i < slen; i++)
+	{
+		sig = (str[i] == 32 || str[i] == '\t') ? 0 : 1;
+		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
+		pre_sig = sig;
+	}
+	if (words == 0)
 		return (NULL);
-	words = (char **) malloc(sizeof(char *) * (w + 1));
-	if (!words)
+	arr_words = malloc(words * sizeof(char *));
+	if (arr_words == NULL)
+	{
+		free(arr_words);
 		return (NULL);
-	w = 0, pos = str;
-	do {
-		while (_isspace(*pos))
-			++pos;
-		if (!*pos)
-			break;
-		for (str = pos++; *pos && !_isspace(*pos); ++pos)
-			;
-		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
-		if (!words[w])
+	}
+	words = 0;
+	for (i = 0; i < slen; i++)
+	{
+		sig = (str[i] == 32 || str[i] == 9) ? 0 : 1;
+		if (sig)
 		{
-			while (w >  0)
-				free(words[--w]);
-			free(words);
-			return (NULL);
+			for (j = 0; str[i + j] != 32 && str[i + j] != 9; j++)
+				;
+			wlen = j;
+			arr_words[words] = malloc(wlen * sizeof(char));
+			if (arr_words[words] == NULL)
+			{
+				for (; words >= 0; words--)
+					free(arr_words[words]);
+				free(arr_words);
+				return (NULL);
+			}
+			for (j = 0; j < wlen; j++)
+			{
+				arr_words[words][j] = str[i + j];
+			}
+			words++;
+			i += wlen - 1;
 		}
-		for (c = 0; str < pos; ++c, ++str)
-			words[w][c] = *str;
-		words[w][c] = '\0';
-	} while (++w, *pos);
-	words[w] = NULL;
-	return (words);
+	}
+	return (arr_words);
 }
