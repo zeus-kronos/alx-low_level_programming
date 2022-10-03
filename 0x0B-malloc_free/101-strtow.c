9471d40bb4b0361ca4d4d7ac2c0706829d3306dc
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-03 15:30:22 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,91 +1,43 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
-* number - function to calculate number of words
-* @str: string being passed to check for words
-*
-* Return: number of words
-*/
-
-int number(char *str)
-{
-	int a, num = 0;
-
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			for (; str[a] != ' ' && str[a] != '\0'; a++)
-				str++;
-			num++;
-		}
-	}
-	return (num);
-}
-
-/**
-* free_everything - frees the memory
-* @string: pointer values being passed for freeing
-* @i: counter
-*/
-
-void free_everything(char **string, int i)
-{
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
-}
-
-/**
-* strtow - function that splits string into words
-* @str: string being passed
-* Return: null if string is empty or null or function fails
+* **strtow - function that splits a string into words.
+* @str : pointer
+* Return: Char
 */
 
 char **strtow(char *str)
 {
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
+	char **d;
+	int i;
+	int j = 0;
+	int con = 0;
 
-	if (str == 0 || *str == 0)
+	if (str == NULL)
 		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (*str == ' ')
-			str++;
+		if (str[i] != 32)
+			con++;
+	}
+
+	d = malloc(sizeof(char) * con);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] != 32)
+		{
+			*d[j] = str[i];
+			j++;
+		}
 		else
 		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-			b++; c = 0; length = 0; str++;
 		}
 	}
-	return (words);
+	return (d);
 }
