b0bd489d3c2d2110257f7ae3fa3e7d33a4deb323
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-04 20:24:05 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -136,7 +136,7 @@ char **strtow(char *str)
 		free(m);
 		return (NULL);
 	}
-	pos = (int *)malloc(sizeof(int) * words * 8);
+	pos = (int *)malloc(sizeof(int) * words * 1.5);
 	if (pos == NULL)
 	{
 		free(m);
