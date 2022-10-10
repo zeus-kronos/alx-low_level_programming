f478abd7a3fbd0161c9a186c7554e18ef3d456ca
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-10 23:48:25 +0100

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point, prints file name it was compiled from
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
