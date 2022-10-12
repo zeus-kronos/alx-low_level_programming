c1a5fa98a7e22a31a7b1c46c6490032a01f33dca
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-12 13:11:31 +0100

    Create 3-calc.h

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,27 @@
+#ifndef OP_FUNCTIONS_
+#define OP_FUNCTIONS_
+
+/**
+* struct op - Struct op
+*
+* @op: The operator
+*
+* @f: Thefunction associated
+*/
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif /* #ifndef OP_FUNCTIONS_ */
