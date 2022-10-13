151d598c57d06017bb0b621280415df15653fff7
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-13 01:31:47 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,14 @@
 * print_name - prints a name
 * @name: name to print
 * @f: pointer to a function
+*
 * Description: prints a name using function pointers
+*
 * Return: void
 */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if(name != NULL && f != NULL)
-		*f(name);
+		f(name);
 }
