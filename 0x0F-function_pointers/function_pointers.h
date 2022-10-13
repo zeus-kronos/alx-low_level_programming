d1bf76412f6fd1d1f353b6deb326580f9ae272ee
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-13 01:06:47 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 #ifndef FUNCTION_POINTERS_
 #define FUNCTION_POINTERS_
 
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
