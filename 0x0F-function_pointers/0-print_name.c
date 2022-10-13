fde0257b46b6b20a2989eb7ec4b69ae25e3fdba1
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-13 01:21:41 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
 * print_name - prints a name
 * @name: name to print
-* @f: function to print with
-* Description: prints a name using function pointers
+* 
 * Return: void
 */
 
