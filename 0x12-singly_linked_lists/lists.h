6aca0f4c2351de5b7885fbe0068d497edbeae60d
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-21 07:16:08 +0100

    Create lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef SINGLY_LINKED_LIST_H_
+#define SINGLY_LINKED_LIST_H_
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* #ifndef SINGLY_LINKED_LIST_H_ */
