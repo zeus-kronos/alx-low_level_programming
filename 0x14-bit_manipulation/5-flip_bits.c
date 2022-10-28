7b564f6714029b913715fd64f13cfdcda2fc900f
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-28 01:15:10 +0100

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* flip_bits - count the bit to change to another value
+* @n: the number
+* @m: the number to compare
+* Description: compare to ints a count the diferences in bits
+* section header: the header of this function is holberton.h
+* Return: the number of different bits
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i;
+
+	i = 0;
+	n = n ^ m;
+	while (n)
+	{
+		i += n & 1;
+		n >>= 1;
+	}
+	return (i);
+}
