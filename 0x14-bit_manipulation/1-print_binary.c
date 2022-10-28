d58e883183ab3d3da4f59f357b086ca0b4d90cc8
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-28 01:10:00 +0100

    Create 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* print_binary - decimal to binary without use / %
+* @n: the decimal
+* Description: convert decimal to binary
+* section header: the header of this function is holberton.h
+* Return: no return
+*/
+void print_binary(unsigned long int n)
+{
+	int flag = 0;
+	unsigned long int mask = 1;
+
+	mask <<= 63;
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && flag == 1)
+			_putchar('0');
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+
+		mask = mask >> 1;
+	}
+}
