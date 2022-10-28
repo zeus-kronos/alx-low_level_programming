3323c95fcac2300400157913097a589ca5c41500
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-28 01:13:50 +0100

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* clear_bit - set a bit to 0
+* @n: the number
+* @index: the position to set the bit
+* Description: set 1 bit to 0
+* section header: the header of this function is holberton.h
+* Return: 1 in success -1 in fail
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
