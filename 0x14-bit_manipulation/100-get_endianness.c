843f976968910c7fad8b4500eee919e246656c7d
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-28 01:16:48 +0100

    Create 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* get_endianness - get the endiannes of the arch
+* Description: get the endiannes of the arch
+* section header: the header of this function is holberton.h
+* Return: 1 in little indian - 0 in big indian.
+*/
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
