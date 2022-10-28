998fd20ec23bf999900aef74e7b4011ccec9553d
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-10-28 01:11:20 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* get_bit - return one bit
+* @n: the number
+* @index: the position to read the bit
+* Description: return 1 bit
+* section header: the header of this function is holberton.h
+* Return: the value of the bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bit;
+
+	if (index > 64)
+		return (-1);
+
+	bit = n >> index;
+
+	return (bit & 0x1);
+}
