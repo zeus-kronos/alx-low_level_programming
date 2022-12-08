ae66d4edc81a3aecca388fac04fb92b7ed7b5361
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-12-08 20:07:58 +0100

    Updated insert dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,12 +5,12 @@ void add_at_the_end(dlistint_t *node_b4_null, dlistint_t *new_node);
 dlistint_t *create_new_node(int n);
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: double pointer to the head of the list
- * @idx: index of the list where the new node should be added
- * @n: elemenent of the new node to be added at @idx
- * Return: the address of the new node, or NULL if it failed
- */
+* insert_dnodeint_at_index - inserts a new node at a given position
+* @h: double pointer to the head of the list
+* @idx: index of the list where the new node should be added
+* @n: elemenent of the new node to be added at @idx
+* Return: the address of the new node, or NULL if it failed
+*/
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -55,10 +55,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 }
 
 /**
- * add_in_middle - adds a node in between nodes of dlistint_t list
- * @node_at_choice_index: pointer to the node at index of insertion
- * @new_node: pointer to new node
- */
+* add_in_middle - adds a node in between nodes of dlistint_t list
+* @node_at_choice_index: pointer to the node at index of insertion
+* @new_node: pointer to new node
+*/
 
 void add_in_middle(dlistint_t *node_at_choice_index, dlistint_t *new_node)
 {
@@ -70,10 +70,10 @@ void add_in_middle(dlistint_t *node_at_choice_index, dlistint_t *new_node)
 }
 
 /**
- * add_at_the_end - adds a node to end of dlistint_t list
- * @node_b4_null: pointer to node before end null node
- * @new_node: pointer to new node
- */
+* add_at_the_end - adds a node to end of dlistint_t list
+* @node_b4_null: pointer to node before end null node
+* @new_node: pointer to new node
+*/
 
 void add_at_the_end(dlistint_t *node_b4_null, dlistint_t *new_node)
 {
@@ -83,10 +83,10 @@ void add_at_the_end(dlistint_t *node_b4_null, dlistint_t *new_node)
 }
 
 /**
- * create_new_node - creates a new node for dlistint_t list
- * @n: elemenent of the new node
- * Return: pointer to a new node
- */
+* create_new_node - creates a new node for dlistint_t list
+* @n: elemenent of the new node
+* Return: pointer to a new node
+*/
 
 dlistint_t *create_new_node(int n)
 {
