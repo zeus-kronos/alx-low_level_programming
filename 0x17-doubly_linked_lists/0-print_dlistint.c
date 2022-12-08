82ad60ddcf1bd850f78cdbead6e777f70c563ca5
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2022-12-08 21:35:30 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: pointer to head of the dlistint_t list
- * Return: number of nodes of dlistint_t list
- */
+* print_dlistint - prints all the elements of a dlistint_t list
+* @h: pointer to head of the dlistint_t list
+* Return: number of nodes of dlistint_t list
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
