14bbf186d3ae8e30c9ec1cd730b929e9ce2264e4
zeus-kronos <zeuskronorz@gmail.com>
2022-12-12 17:19:05 +0100

    Added dynamic libraries files

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+/**
+* add - Computes the sum of two numbers
+* @left: The left-sided number
+* @right: The right-sided number
+*
+* Return: Result
+*/
+
+int add(int left, int right)
+{
+	return (left + right);
+}
+
+/**
+* sub - Computes the difference of two numbers
+* @left: The left-sided number
+* @right: The right-sided number
+*
+* Return: Result
+*/
+
+int sub(int left, int right)
+{
+	return (left - right);
+}
+
+/**
+ * mul - Computes the product of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
+ */
+
+int mul(int left, int right)
+{
+	return (left * right);
+}
+
+/**
+ * div - Computes the quotient of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
+ */
+
+int div(int left, int right)
+{
+	return (left / right);
+}
+
+/**
+ * mod - Computes the modulo of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: Result
+ */
+
+int mod(int left, int right)
+{
+	return (left % right);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,12 @@
+## C - Dynamic libraries
+
+### Learning Objectives
+
+At the end of the project, you are expected to be able to explain to anyone, without the help of Google:
+
+#### General
+
+ - What is a dynamic library, how does it work, how to create one, and how to use it
+ - What is the environment variable `$LD_LIBRARY_PATH` and how to use it
+ - What are the differences between static and shared libraries
+ - Basic usage nm, ldd, ldconfig
