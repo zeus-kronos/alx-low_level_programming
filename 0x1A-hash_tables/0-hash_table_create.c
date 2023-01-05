1fe7d9b501961ce2e2f0f3954581b47e440c511b
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2023-01-05 21:59:05 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table (as name implies) of size @size
- *
- * @size: The size of the hash table (the spaces allocated for nodes)
- * Return: pointer to the hash table on success, else, null
- */
+* hash_table_create - creates a hash table (as name implies) of size @size
+*
+* @size: The size of the hash table (the spaces allocated for nodes)
+* Return: pointer to the hash table on success, else, null
+*/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
