9422e3b2405fed9d1755e6aa734b14e284e03364
Gabriel Dan <111608696+zeus-kronos@users.noreply.github.com>
2023-01-05 21:58:37 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
- * @ht: pointer to the hash table
- * Return: void
- */
+* hash_table_delete - deletes a hash table
+* @ht: pointer to the hash table
+* Return: void
+*/
 
 void hash_table_delete(hash_table_t *ht)
 {
